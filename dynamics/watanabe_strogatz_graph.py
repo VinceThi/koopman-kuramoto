0aa459c7538046b04b8eedcfc79beee07d215053
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2024-03-14 11:14:06 -0400

    partially debugged ws_equations_graph

--- dynamics/watanabe_strogatz_graph.py
+++ dynamics/watanabe_strogatz_graph.py
@@ -17,6 +17,10 @@ def Z_dot_g(Z, omegas_Z, p_1, p_m1):
     return 1j*omegas_Z * Z + p_1 - p_m1 * Z**2
 
 
+def phi_dot_g(Z, omegas_Z, p_m1):
+    return omegas_Z - 2 * np.imag(p_m1 * Z)
+
+
 def z_dot_g(z, omegas_z, adj_matrix, z_and_zeta):
     n = len(z)
     q = (adj_matrix @ z_and_zeta)[0:n]
@@ -24,11 +28,7 @@ def z_dot_g(z, omegas_z, adj_matrix, z_and_zeta):
     return 1j * omegas_z * z + q - z**2 * q_tilde
 
 
-def phi_dot_g(Z, omegas_Z, p_m1):
-    return omegas_Z - 2 * np.imag(p_m1 * Z)
-
-
-def ws_equations_graph(t, state, w_allparts, omegas_Z, omegas_z, adj_matrix):
+def ws_equations_graph(t, state, w_allparts, omegas_Z, omegas_z, adj_matrix, adj_matrix_intparts):
     # extract the variables from the arguments
     Z = np.array([state[:len(omegas_Z)]]).T
     phi = np.array([state[len(omegas_Z):2*len(omegas_Z)]]).T
@@ -41,27 +41,11 @@ def ws_equations_graph(t, state, w_allparts, omegas_Z, omegas_z, adj_matrix):
         zeta += zeta_partmu.tolist()
     zeta = np.array([zeta]).T
     z_and_zeta = np.concatenate((z, zeta), axis=0)
-    adj_matrix_reduced = np.concatenate((adj_matrix[:len(z[0])], np.unique(adj_matrix[len(z[0]):], axis=0)), axis=0)
-    p_1 = (adj_matrix_reduced @ z_and_zeta)[-len(Z):]
-    p_m1 = (adj_matrix_reduced @ z_and_zeta**(-1))[-len(Z):]
+    p_1 = (adj_matrix_intparts @ z_and_zeta)
+    p_m1 = (adj_matrix_intparts @ z_and_zeta**(-1))
 
     derivatives = [Z_dot_g(Z, omegas_Z, p_1, p_m1), phi_dot_g(Z, omegas_Z, p_m1), z_dot_g(z, omegas_z, adj_matrix, z_and_zeta)]
     derivatives = np.concatenate(list(map(lambda x: x.flatten(), derivatives)))
     return derivatives
 
 
-
-
-
-
-
-    # Z, phi, z = state
-    # zeta = []
-    # for mu, Z_mu in enumerate(Z):
-        # zeta.append(ws_transformation(Z_mu, phi[mu], w_allparts[mu]))
-    # zeta_and_z = np.concatenate(np.concatenate(zeta), z)
-
-    # p_1 = np.sum(adj_matrix @ zeta_and_z)    # WARNING: Make sure that adj matrix is correctly formatted
-    # p_m1 = np.sum(adj_matrix @ zeta_and_z**(-1))
-
-    # return np.array([Z_dot(Z, omegas, p_1, p_m1), phi_dot(Z, omegas, p_m1)]) # TODO: ADD z_dot
