534e03eb9d03ced0d6420530a6e42c0f64f24e9b
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2024-03-26 11:19:45 -0400

    added jacobian function for ws_initial_conditions

--- dynamics/ws_initial_conditions.py
+++ dynamics/ws_initial_conditions.py
@@ -1,3 +1,4 @@
+from math import cos
 import numpy as np
 from scipy.optimize import root
 from scipy.sparse import csr_matrix
@@ -16,8 +17,8 @@ from tqdm import tqdm
 def objective_function_init_cond(x, theta0):
     """ R, Theta, Phi, psi_1, ..., psi_N = x
      where Z = R e^(i Theta), w_j = e^(i psi_j), Phi = Theta - phi """
-    return np.concatenate([((np.cos(theta0) + x[0]*np.cos(x[3:] + theta0 - x[2]) - np.cos(x[1] - x[2] + x[3:]) - x[0]*np.cos(x[1]))**2 +
-                           (np.sin(theta0) + x[0]*np.sin(x[3:] + theta0 - x[2]) - np.sin(x[1] - x[2] + x[3:]) - x[0]*np.sin(x[1]))**2)**(1/2),
+    return np.concatenate([(np.cos(theta0) + x[0]*np.cos(x[3:] + theta0 - x[2]) - np.cos(x[1] - x[2] + x[3:]) - x[0]*np.cos(x[1]))**2 +
+                           (np.sin(theta0) + x[0]*np.sin(x[3:] + theta0 - x[2]) - np.sin(x[1] - x[2] + x[3:]) - x[0]*np.sin(x[1]))**2,
                            np.array([np.sum(np.sin(x[3:])), np.sum(np.cos(x[3:])), x[3]])])
 
 # OLD VERSION
@@ -39,6 +40,32 @@ def objective_function_init_cond(x, theta0):
     # return dfdx
 
 
+def jacobian_matrix_objective_function(x, theta0):
+    """ Define Jacobian and add non zero entries.
+     R, Theta, Phi, psi_1, ..., psi_N = x
+     where Z = R e^(i Theta), w_j = e^(i psi_j), Phi = Theta - phi """
+    N = len(theta0)
+    d = len(x)  # = N + 3
+    dfdx = np.zeros((d, d))
+    cos_term = np.cos(theta0) + x[0]*np.cos(x[3:] + theta0 - x[2]) - np.cos(x[1] - x[2] + x[3:]) - x[0]*np.cos(x[1])
+    sin_term = np.sin(theta0) + x[0]*np.sin(x[3:] + theta0 - x[2]) - np.sin(x[1] - x[2] + x[3:]) - x[0]*np.sin(x[1])
+
+    dfdx[0, :N] = 2 * cos_term * (np.cos(x[3:] + theta0 - x[2]) - np.cos(x[1])) + \
+                  2 * sin_term * (np.sin(x[3:] + theta0 - x[2]) - np.sin(x[1]))
+    dfdx[1, :N] = 2 * cos_term * (np.sin(x[1] - x[2] + x[3:]) + x[0]*np.sin(x[1])) + \
+                  2 * sin_term * (-np.cos(x[1] - x[2] + x[3:]) - x[0]*np.cos(x[1]))
+    dfdx[2, :N] = 2 * cos_term * (x[0]*np.sin(x[3:] + theta0 - x[2]) - np.sin(x[1] - x[2] + x[3:])) + \
+                  2 * sin_term * (-x[0]*np.cos(x[3:] + theta0 - x[2]) + np.cos(x[1] - x[2] + x[3:]))
+    dfdx[3:, :N] = np.diag(2 * cos_term * (-x[0]*np.sin(x[3:] + theta0 - x[2]) + np.sin(x[1] - x[2] + x[3:])) + \
+                           2 * sin_term * (x[0]*np.cos(x[3:] + theta0 - x[2]) - np.cos(x[1] - x[2] + x[3:])))
+
+    dfdx[3:, N] = np.cos(x[3:])
+    dfdx[3:, N+1] = -np.sin(x[3:])
+    dfdx[3, N+2] = 1
+
+    return dfdx
+
+
 def get_watanabe_strogatz_initial_conditions(theta0, N, dispersed_guess=False, nb_guess=1, tol=1e-10):
     """Warning: since the objective function is squared, the tolerance specified here is smaller than the actual difference."""
     """ Warning (OLD): Choosing a too low tolerance can cause problems, the unit test
@@ -55,7 +82,7 @@ def get_watanabe_strogatz_initial_conditions(theta0, N, dispersed_guess=False, n
         Phi = 2*np.pi*np.random.random()
         psi = 2*np.pi*np.random.random(N)
         initial_guess = np.concatenate([np.array([R, Theta, Phi]), psi])
-        solution = root(objective_function_init_cond, initial_guess,
+        solution = root(objective_function_init_cond, initial_guess, jac=jacobian_matrix_objective_function,
                         args=(theta0,), method='hybr', options={'xtol': tol, 'col_deriv': True})
         if solution.success:
             if 0 < solution.x[0] < 1:
--- tests/test_dynamics/run_ws_initial_conditions.py
+++ tests/test_dynamics/run_ws_initial_conditions.py
@@ -1,13 +1,17 @@
 import numpy as np
 from dynamics.watanabe_strogatz import ws_transformation
 from dynamics.ws_initial_conditions import get_watanabe_strogatz_initial_conditions
+import time
 
 
-print("\nBeginning test_get_watanabe_strogatz_initial_conditions...")
+begin = time.time()
 N = 1000
-np.random.seed(20)
+np.random.seed(57)
 theta0 = 2*np.pi*np.random.random(N)
 z0 = np.exp(1j*theta0)
 Z0, phi0, w = get_watanabe_strogatz_initial_conditions(theta0, N, nb_guess=100)
 print("|z0 - ws_transformation(Z0, phi0, w)| = ", np.abs(z0 - ws_transformation(Z0, phi0, w)))
 assert np.all(np.abs(z0 - ws_transformation(Z0, phi0, w)) < 1e-6)
+end = time.time()
+
+print('total time', end - begin)
