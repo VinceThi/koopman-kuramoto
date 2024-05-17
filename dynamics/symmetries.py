e5159aa0e9571bd0c466735e14e431413389fcb4
VinceThi <vincent.thibeault.1@ulaval.ca>
2024-05-17 08:12:16 -0400

    Update code to verify symmetries, it will work !

--- dynamics/symmetries.py
+++ dynamics/symmetries.py
@@ -1,78 +1,6 @@
 import numpy as np
 
 
-def rfun(Z, Zbar, phi):
-    zeta = np.exp(-1j*phi)
-    x = 1 + zeta
-    y = np.sqrt((1 - zeta)**2 + 4*zeta*Z*Zbar)
-    ratio = (x + y)/(x - y)
-    module = np.abs(ratio)
-    argument = np.angle(ratio)
-    return (np.log(module) + 1j*argument)/y
-    # return np.log((x + y)/(x - y))/y
-
-
-def infinitesimal_condition_symmetry_kuramoto(t, state, p1, current_index, omega):
-    Z, Zbar, phi = state
-    r = rfun(Z, Zbar, phi)
-    normZ2 = Z*Zbar
-    zeta = np.exp(-1j*phi)
-    zetabar = np.exp(1j*phi)
-    gamma = (1 - zeta)**2 + 4*zeta*Z*Zbar
-    alpha = (1 - zetabar - r*zeta + r - 2*r*Z*Zbar)/gamma
-    beta = ((1 + zeta)/(1 - Z*Zbar) - 2*r*zeta)/gamma
-    k = current_index
-    pm1 = np.conjugate(p1[k])
-
-    A = np.array([[r + alpha*(zeta-1) + beta*normZ2, - beta*Zbar**2, beta*(1 - zeta)*Zbar],
-                  [-beta*zetabar*Z**2, r + alpha*(zeta - 1) + beta*zetabar*normZ2, beta*(1 - zeta)*Z],
-                  [-(r*zetabar + alpha + beta*zetabar*normZ2)*Z, (beta*zetabar*normZ2 - alpha)*Zbar, r+2*beta*normZ2]])
-    b = np.array([1j*omega*Z + (1-zetabar)*p1[k], -1j*omega*Zbar + (1-zeta)*pm1, 2*pm1*Z*zeta - 2*p1[k]*Zbar])
-    dZdt, dZbardt, dzetadt = (1-normZ2)*zeta*(A@b)
-    return np.array([dZdt, dZbardt, np.angle(dzetadt)])
-
-
-def infinitesimal_condition_symmetry_kuramoto_2(t, state, p1, current_index, omega):
-    X, Y, phi = state
-    Z = X + 1j*Y
-    Zbar = X - 1j*Y
-    normZ2 = X**2 + Y**2
-    r = rfun(Z, Zbar, phi)
-    zeta = np.exp(-1j*phi)
-    zetabar = np.exp(1j*phi)
-    gamma = (1 - zeta)**2 + 4*zeta*normZ2
-    alpha = (1 - zetabar - r*zeta + r - 2*r*normZ2)/gamma
-    beta = ((1 + zeta)/(1 - normZ2) - 2*r*zeta)/gamma
-    k = current_index
-    pm1 = np.conjugate(p1[k])
-
-    A = np.array([[1j*beta*(1 - zetabar)*X*Y + r + alpha*(zeta - 1) + beta*(1 + zetabar)*Y**2,
-                   beta*X*(1j*X*(zetabar-1)-Y*(zetabar+1)),
-                   2*beta*(1 - zeta)*X],
-                  [beta*Y*(1j*Y*(1 - zetabar) - X*(zetabar + 1)),
-                   1j*beta*(zetabar - 1)*X*Y + r + alpha*(zeta - 1) + beta*(1 + zetabar)*X**2,
-                   2*beta*(1 - zeta)*Y],
-                  [-2*alpha*X - r*zetabar*(X + 1j*Y) - 2*1j*beta*zetabar*Y*normZ2,
-                   -2*alpha*Y + 1j*r*zetabar*(X + 1j*Y) + 2*1j*beta*zetabar*X*normZ2,
-                   2*r+4*beta*normZ2]])
-    b = np.array([-omega*Y + ((1-zetabar)*p1[k] + (1-zeta)*pm1)/2,
-                  omega*X + ((1-zetabar)*p1[k] - (1-zeta)*pm1)/(2*1j),
-                  (pm1*zeta - p1[k])*X + 1j*(pm1*zeta + p1[k])*Y])
-    dXdt, dYdt, dzetadt = (1-normZ2)*zeta*(A@b)
-    return np.array([dXdt, dYdt, np.angle(dzetadt)])
-
-
-def determining_equations_disk_automorphism(t, state, theta, current_index, omega, coupling):
-    V, Y = state
-    Vbar = np.conjugate(V)
-    p0 = len(theta[0, :])*coupling/2
-    p1 = coupling/2*np.sum(np.exp(1j*theta[current_index, :]))
-    p2 = coupling/2*np.sum(np.exp(2*1j*theta[current_index, :]))
-    dVdt = (p0 + 1j*omega)*V - p2*Vbar
-    dYdt = 1j*(np.conjugate(p1)*V - p1*Vbar)
-    return np.array([dVdt, dYdt])
-
-
 def nu_function(R, Y):
     X = np.sqrt(R**2 - Y**2 + 1)
     Gamma = np.sqrt(X**2 - 1)
@@ -95,30 +23,31 @@ def determining_equations_real_disk_automorphism(t, state, theta, current_index,
     dYdt = -mu*Y - chi1*R
     return np.array([dRdt, dPhidt, dYdt])
 
-# def nu_function(R, Y, Phi):
-#     Gamma = np.sqrt(X**2 - 1)
-#     ratio = (X + Gamma)/(X - Gamma)
-#     return np.log(ratio)/(2*Gamma)
-#
-#
-# def mu_function(X, nu):
-#     return (1 - nu*X)/(X**2 - 1)
-#
-#
-# def determining_equations_real_disk_automorphism(t, state, theta, current_index, omega, coupling):
-#     R, Phi, X, Y = state
-#     nu = nu_function(X)
-#     mu = mu_function(X, nu)
+
+# def determining_equations_real_disk_automorphism(t, state, timesteps, theta, current_index, omega, coupling):
+#     R, Phi, Y = state
+# 
+#     # Find the index corresponding to the current time step
+#     index = np.searchsorted(timesteps, t) - 1
+#     if index < 0:
+#         index = 0
+#     elif index >= len(theta[0, :]):
+#         index = len(theta[0, :]) - 1
+# 
+#     # Get the value of the non-autonomous parts at the current time step
+#     theta_t = theta[index, :]
+# 
 #     p0 = len(theta[0, :])*coupling/2
-#     p1 = coupling/2*np.sum(np.exp(1j*theta[current_index, :]))
-#     p2 = coupling/2*np.sum(np.exp(2*1j*theta[current_index, :]))
+#     p1 = coupling/2*np.sum(np.exp(1j*theta_t))
+#     p2 = coupling/2*np.sum(np.exp(2*1j*theta_t))
 #     rho1, phi1 = np.abs(p1), np.angle(p1)
 #     rho2, phi2 = np.abs(p2), np.angle(p2)
-#     chi1 = 2*rho1*np.sin(2*Phi - phi1)
+#     chi1 = 2*rho1*np.sin(Phi - phi1)
 #     chi2 = p0 - rho2*np.cos(2*Phi - phi2)
-#     dRdt = chi2*R - mu*chi1*R**2 - mu*chi2*R**3
+#     mu = (1 - nu_function(R, Y)*np.sqrt(R**2 - Y**2 + 1))/(R**2 - Y**2)*(chi1*Y*R + chi2*R**2)
+#     dRdt = (chi2 - mu)*R
 #     dPhidt = omega + rho2*np.sin(2*Phi - phi2)
-#     dXdt = nu*(chi1*Y*R + chi2*R**2)
-#     dYdt = -chi1*R - mu*(chi1*R*Y**2 + chi2*R**2*Y)
-#     return np.array([dRdt, dPhidt, dXdt, dYdt])
-#
+#     dYdt = -mu*Y - chi1*R
+#     return np.array([dRdt, dPhidt, dYdt])
+# 
+# 
--- simulations/symmetry_disk_automorphism.py
+++ simulations/symmetry_disk_automorphism.py
@@ -15,12 +15,12 @@ N = 4
 W = np.ones((N, N))
 
 """ Dynamical parameters """
-t0, t1, dt = 0, 12, 0.001
+t0, t1, dt = 0, 12, 0.0001
 timelist = np.linspace(t0, t1, int(t1 / dt))
 alpha = 0
 omega = 1
 coupling = 0.5/N
-theta0 = np.array([0, 2, 4, 6])  # np.random.random(N)
+theta0 = np.array([0, 2, 4, 6])  # 2*np.pi*np.random.random(N)  #
 
 """ Integrate Kuramoto model """
 args_dynamics = (W, coupling, omega, alpha)
@@ -29,30 +29,9 @@ theta = np.array(integrate_dopri45(t0, t1, dt, kuramoto_sakaguchi, theta0, *args
 """ Integrate determining equations """
 args_determining = (omega, coupling)
 
-# Take 1
-# X = 2
-# Y0 = 0.5
-# V0 = np.sqrt(X**2 + Y0**2 - 1)*np.exp(1j*0.2)
-# solution = np.array(integrate_dopri45_non_autonomous(t0, t1, dt, determining_equations_disk_automorphism,
-#                                                      np.array([V0, Y0]), theta, *args_determining))
-# V, Y = solution[:, 0], solution[:, 1]
-
-# Take 2
-# R0 = 0.2
-# Phi0 = np.pi/5
-# X0 = 1 + 1e-8
-# # assert 1 < X0 < np.sqrt(R0**2 + 1)
-# # R0 = np.sqrt(X0**2 - 1) + 0.01
-# Y0 = -np.sqrt(1 + R0**2 - X0**2)
-# print([R0, Phi0, X0, Y0])
-# solution = np.array(integrate_dopri45_non_autonomous(t0, t1, dt, determining_equations_real_disk_automorphism,
-#                                                      np.array([R0, Phi0, X0, Y0]), theta, *args_determining))
-# R, Phi, X, Y = solution[:, 0], solution[:, 1], solution[:, 2], solution[:, 3]
-
-# Take 3
-R0 = 0.2
+R0 = 0.5
 Phi0 = np.pi/5
-Y0 = 0.01
+Y0 = 0.1
 assert R0 > Y0
 
 solution = np.array(integrate_dopri45_non_autonomous(t0, t1, dt, determining_equations_real_disk_automorphism,
@@ -162,3 +141,159 @@ plt.xlabel("Time $t$")
 plt.legend()
 
 plt.show()
+
+
+
+
+
+# # -*- coding: utf-8 -*-
+# # @author: Vincent Thibeault
+#
+# from plots.config_rcparams import *
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.integrate import solve_ivp
+# from dynamics.dynamics import kuramoto_sakaguchi
+# from dynamics.symmetries import determining_equations_real_disk_automorphism
+#
+# plot_trajectories = True
+#
+# """ Graph parameters """
+# N = 4
+# W = np.ones((N, N))
+#
+# """ Dynamical parameters """
+# t0, t1 = 0, 12
+# t_span = [t0, t1]
+# integration_method = 'BDF'
+# rtol = 1e-8
+# atol = 1e-12
+#
+# alpha = 0
+# omega = 1
+# coupling = 0.5/N
+# theta0 = np.array([0, 2, 4, 6])  # 2*np.pi*np.random.random(N)  #
+#
+# """ Integrate Kuramoto model """
+# args_dynamics = (W, coupling, omega, alpha)
+# sol = solve_ivp(kuramoto_sakaguchi, t_span, theta0, integration_method, args=args_dynamics,
+#                 rtol=rtol, atol=atol, vectorized=True)  # ,  jac=jacobian_complete)
+# theta = sol.y
+# # theta = np.array(integrate_dopri45(t0, t1, dt, kuramoto_sakaguchi, theta0, *args_dynamics))
+#
+# """ Integrate determining equations """
+# args_determining = (omega, coupling)
+#
+# R0 = 0.5
+# Phi0 = np.pi/5
+# Y0 = 0.1
+# assert R0 > Y0
+#
+# solution = np.array(integrate_dopri45_non_autonomous(t0, t1, dt, determining_equations_real_disk_automorphism,
+#                                                      np.array([R0, Phi0, Y0]), theta, *args_determining))
+# R, Phi, Y = solution[:, 0], solution[:, 1], solution[:, 2]
+# X = np.sqrt(R**2 - Y**2 + 1)
+#
+# U = X + 1j*Y
+# V = R*np.exp(1j*Phi)
+#
+# Z = V/U
+# phi = np.angle(-U/np.abs(U))
+#
+# """ Transform the solution theta(t) into another hattheta(t) """
+#
+#
+# def disk_automorphism(U, V, z): return (U*z + V)/(np.conjugate(V)*z + np.conjugate(U))
+# # def disk_automorphism(U, V, z): return (np.conjugate(U)*z - V)/(-np.conjugate(V)*z + U)   # Inverse of above
+#
+#
+# hattheta = []
+# for i in range(len(timelist)):
+#     hattheta.append(np.angle(disk_automorphism(U[i], V[i], np.exp(1j*theta[i, :]))))
+# hattheta = np.array(hattheta)
+#
+#
+# """ For the initial conditions of the transformed system, what is the expected dynamics ? """
+# hattheta0 = hattheta[0, :]
+# hattheta_expected = np.array(integrate_dopri45(t0, t1, dt, kuramoto_sakaguchi, hattheta0, *args_dynamics))
+#
+#
+# """ Compute the vector fields to see if we indeed obtained solutions of the Kuramoto model """
+# vector_field_theta = []
+# vector_field_hattheta = []
+# for i in range(len(timelist)):
+#     vector_field_theta.append(kuramoto_sakaguchi(0, theta[i, :], W, coupling, omega, alpha))  # To test
+#     vector_field_hattheta.append(kuramoto_sakaguchi(0, hattheta[i, :], W, coupling, omega, alpha))
+# vector_field_theta = np.array(vector_field_theta)
+# vector_field_hattheta = np.array(vector_field_hattheta)
+#
+# """ Compute the time derivative """
+# time_derivative_theta = np.diff(theta, axis=0)/dt
+# time_derivative_hattheta = np.diff(hattheta, axis=0)/dt
+#
+#
+# """ Compare the time derivatives vs. the vector fields and the trajectories vs. the transformed trajectories """
+# plt.figure(figsize=(14, 10))
+# plt.subplot(231)
+# theta = theta % (2*np.pi)
+# hattheta = np.where(hattheta < 0, 2*np.pi + hattheta, hattheta)
+# hattheta_expected = hattheta_expected % (2*np.pi)
+# for i in range(len(theta[0, :])):
+#     if i == 0:
+#         plt.plot(timelist, theta[:, i], color=deep[0], label="Solution $\\theta(t)$")
+#         plt.plot(timelist, hattheta[:, i], color=deep[1],
+#                  linestyle="--", label="Transformed solution $\\hat{\\theta}(t)$")
+#         plt.plot(timelist, hattheta_expected[:, i], color=deep[2],
+#                  linestyle="--", label="Expected $\\hat{\\theta}(t)$")
+#     else:
+#         plt.plot(timelist, theta[:, i], color=deep[0])
+#         plt.plot(timelist, hattheta[:, i], color=deep[1], linestyle="--")
+#         plt.plot(timelist, hattheta_expected[:, i], color=deep[2], linestyle="--")
+# plt.ylabel("Phase")
+# plt.xlabel("Time $t$")
+# plt.legend(loc=1, frameon=True)
+#
+# plt.subplot(234)
+# for i in range(len(theta[0, :])):
+#     if i == 0:
+#         plt.plot(time_derivative_theta[:, i], color=deep[0], label="Derivative $\\theta$")
+#         plt.plot(vector_field_theta[:, i], color=deep[9], linestyle="--", label="$F(\\theta)$")
+#         plt.plot(time_derivative_hattheta[:, i], color=deep[4], label="Derivative $\\hat{\\theta}$")
+#         plt.plot(vector_field_hattheta[:, i], color=deep[6], linestyle="--", label="$F(\\hat{\\theta}$)")
+#     else:
+#         plt.plot(time_derivative_theta[:, i], color=deep[0])
+#         plt.plot(vector_field_theta[:, i], color=deep[9], linestyle="--")
+#         plt.plot(time_derivative_hattheta[:, i], color=deep[4])
+#         plt.plot(vector_field_hattheta[:, i], color=deep[6], linestyle="--")
+# plt.ylim([-0.5, 3])
+# plt.xlabel("Timepoints")
+# plt.legend()
+#
+# plt.subplot(232)
+# plt.plot(np.real(U), np.imag(U), label="$U$")
+# plt.plot(np.real(V), np.imag(V), label="$V$")
+# plt.plot(np.real(Z), np.imag(Z), label="$Z$")
+# plt.legend()
+#
+# plt.subplot(235)
+# plt.plot(timelist, np.abs(U), label="$|U|$")
+# plt.plot(timelist, np.abs(V), label="$|V|$")
+# plt.plot(timelist, np.abs(U)**2 - np.abs(V)**2, label="$|U|^2 - |V|^2$")
+# plt.xlabel("Time $t$")
+# plt.legend()
+#
+# plt.subplot(233)
+# plt.plot(timelist, R, label="$R$")
+# plt.plot(timelist, Phi, label="$\\Phi$")
+# plt.plot(timelist, phi, label="$\\phi$")
+# plt.xlabel("Time $t$")
+# plt.legend()
+#
+# plt.subplot(236)
+# plt.plot(timelist, X, label="$X$")
+# plt.plot(timelist, Y, label="$Y$")
+# plt.xlabel("Time $t$")
+# plt.legend()
+#
+# plt.show()
+#
