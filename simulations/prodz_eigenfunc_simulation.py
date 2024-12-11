8054137f23928a1b6989a122725f6eec7623e2b7
Benjamin Claveau <benjamin.claveau.1@ulaval.ca>
2024-12-11 15:30:30 -0500

    minor changes

--- simulations/prodz_eigenfunc_simulation.py
+++ simulations/prodz_eigenfunc_simulation.py
@@ -10,7 +10,7 @@ from dynamics.integrate import integrate_dopri45
 # set dynamical parameters
 N = 3
 ones_vec = np.ones((N, 1))
-w_coeffs = np.array([[6., 10., -14.]])
+w_coeffs = np.array([[1., 1., 1.]])
 w_matrix = ones_vec @ w_coeffs
 np.fill_diagonal(w_matrix, 0)
 print(w_matrix)
@@ -21,7 +21,7 @@ alpha = 0
 # integrate the dynamics
 x0 = 2*np.pi*np.random.random(N)
 print(x0)
-t0, t1, dt = 0, 30, 0.001
+t0, t1, dt = 0, 15, 0.001
 time = np.linspace(t0, t1, int(t1 / dt))
 
 args_dynamics = (w_matrix, coupling, omega, alpha)
@@ -40,12 +40,15 @@ eigenfunc1 = mu1 @ theta.T
 plt.figure()
 for j in range(0, N):
     plt.plot(time, (theta[:, j]) % (2*np.pi), linewidth=0.5)
-plt.plot(time, eigenfunc1[0] % (2*np.pi), linewidth=1, label='eigenfunc')
+plt.plot(time, eigenfunc1[0] % (2*np.pi), linewidth=1, label='eigenfunc1')
+plt.plot(time, (eigenfunc1[0] + 2*np.pi/3) % (2*np.pi), linewidth=1, label='eigenfunc2')
+plt.plot(time, (eigenfunc1[0] + 2*np.pi*2/3) % (2*np.pi), linewidth=1, label='eigenfunc3')
 # plt.plot(time, (2*eigenfunc2[0]/(k2*np.sum(w_coeffs))) % (2*np.pi), linewidth=1, label='eigenfunc')
 ylab = plt.ylabel('$\\theta_j$', labelpad=20)
 ylab.set_rotation(0)
 plt.xlabel('Time $t$')
 # plt.ylim([-1, 2*np.pi + 1])
 plt.tick_params(axis='both', which='major')
+plt.legend()
 plt.tight_layout()
 plt.show()
